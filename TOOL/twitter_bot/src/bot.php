5759dfb04ba9b531d0fa7f2026f362c98c043d56
Seiya-Tagami <bepozurapu274@gmail.com>
2022-12-17 17:22:05 +0900

    feat: composerをインストール、twitterOAuthを導入

--- TOOL/twitter_bot/src/bot.php
+++ TOOL/twitter_bot/src/bot.php
@@ -1,6 +1,17 @@
 <?php
 
-require_once(__DIR__ .'/config.php');
+require_once(__DIR__ . '/config.php');
 
 //package
 // - Composer
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+$connection->setApiVersion("2");
+// $content = $connection->get("account/verify_credentials");
+// $content = $connection->get("statuses/home_timeline");
+$response = $connection->post('tweets', ['text' => '本日のテスト'], true);
+var_dump($response);
+
+// 結論：何かとめんどくさい
+
